4dfcb0751194a273f868c41b3ab9c01332963219
Ivan Pereira <ivanexdev@gmail.com>
2026-01-02 14:01:02 -0300

    Update[IT-190]: fix word bundle

--- Sources/TravelinDesignSystem/Tokens/Colors.swift
+++ Sources/TravelinDesignSystem/Tokens/Colors.swift
@@ -43,11 +43,10 @@ public extension DesignTokens {
         public static let secondaryBackground: Color = Color("secondaryBackground", bundle: .module)
 
         public static let tertiaryBackground: Color = Color("tertiaryBackground", bundle: .module)
-        
-        public static let gradientColor1 : Color = Color("gradientBackground1", bunlde: .module)
-        
-        public static let gradientColor2 : Color = Color("gradientBackground2", bunlde: .module)
-        
+
+        public static let gradientColor1: Color = Color("gradientBackground1", bundle: .module)
+
+        public static let gradientColor2: Color = Color("gradientBackground2", bundle: .module)
 
         // --- UI / Borders ---
 
